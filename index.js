ad1bc23a1fbac7464ba8ce83c6c9b49224ef2b3a
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-07 03:52:56 +0600

    add delete api

--- index.js
+++ index.js
@@ -121,6 +121,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE: Remove an artwork
+    app.delete("/artworks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artWorks.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
